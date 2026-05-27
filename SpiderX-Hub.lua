dda46c7e764a46b24be589aa2735445fc48bf9c8
MethodV2 <fadfdel689@gmail.com>
2026-05-27 15:33:06 +0300

    Update SpiderX-Hub.lua

--- SpiderX-Hub.lua
+++ SpiderX-Hub.lua
@@ -66,7 +66,7 @@ end)
 aillgames.MouseButton1Click:Connect(function()
 Frame:Destroy()
 wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MethodV2/Spider/refs/heads/main/rbx.lua"))()
+loadstring(game:HttpGet("https://github.com/MethodV2/Spider/raw/refs/heads/main/T70N.lua"))()
 end)
 
 return ScreenGui
